950c4a53887dd02083148c00b34dcac028080924
Bang1999 <bangdh1593@gmail.com>
2024-10-01 20:28:26 +0900

    사용자 정의 고객 만들기

---
+++ springsecsection4/src/main/java/com/eazybytes/config/EazyBankUserDetailService.java
@@ -0,0 +1,38 @@
+package com.eazybytes.config;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EazyBankUserDetailService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    /**
+     * @param username the username identifying the user whose data is required.
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details를 찾을 수 없습니다: " + username));
+        log.info("{}", customer);
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
---
+++ springsecsection4/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrfConfig -> csrfConfig.disable());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsServic1e(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * 사용자 비밀 번호가 유출 되었는지 확인하는 메소드
+     * From Spring Security 6.3부터 도입
+     * @return
+     * */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
